6a7c746f6d698ed5de26705aafd4b6a16195d3aa
IMRAN BAALI <imran.baali19@gmail.com>
2020-05-28 17:27:17 +0000

    change in learnGit.bash

--- learnGit.bash
+++ learnGit.bash
@@ -6,3 +6,9 @@ git commit -am "comment" # express commit equivalent to {git add; git commit -m 
 git status # show files status in repo
 git log # show log like commit
 git show # show changed that make in commit
+git reset HEAD license.md # to cancel the last staged change of file license.md
+git checkout -- license.md # to remove the unstaged change maked by git reset head of file license.md
+git help log # to get help for log option
+git config --global --list # to get all variale in git config
+git --global alias.hist "log --oneline --graph --decorate --all" # to make an alias for a git commande
+
